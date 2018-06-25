762109cb4b69a8027f11dffa33b72a3d2db91625
Lucas G. Zhang <lucas@aicoaico.com>
2018-06-26 03:09:46 +0800

    project changed

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/pages/home/Home.vue";
+import List from "@/pages/list/List.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/list",
+      name: "List",
+      component: List
     }
   ]
-})
+});
